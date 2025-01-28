137ffd9ff8f991a3c4bac44336b82b8226c803fa
michel-racine <91474769+michel-racine@users.noreply.github.com>
2025-01-28 11:28:54 -0500

    Update IAggregate.cs

--- Aggregates/IAggregate.cs
+++ Aggregates/IAggregate.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Assi1.Aggregates
+﻿namespace Assi1.Aggregates
 {
     // Interface for collections that support iteration
     public interface IAggregate
